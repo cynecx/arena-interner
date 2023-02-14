d45c32110cc7a5d2e37906bab5d5a24222f25d2b
Paul Nguyen <paul@joinpult.com>
2023-02-14 18:31:18 +0100

    add send/sync tests

---
+++ tests/ui/send_sync.rs
@@ -0,0 +1,22 @@
+use arena_interner::make_interner;
+
+fn is_send<T: Send>(val: T) {}
+fn is_sync<T: Sync>(val: T) {}
+
+fn typed() {
+    make_interner!(mut interner);
+    let mut interner = interner.typed_interner::<i32>();
+    is_send(&interner); // should fail
+    is_sync(&interner); // should fail
+    is_send(&mut interner);
+    is_sync(&mut interner); // should fail
+}
+
+fn main() {
+    make_interner!(mut interner);
+    is_send(&interner); // should fail
+    is_sync(&interner); // should fail
+    is_send(&mut interner);
+    is_sync(&mut interner); // should fail
+    typed();
+}
